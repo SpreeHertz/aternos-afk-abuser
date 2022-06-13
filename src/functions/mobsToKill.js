f6826f564d9e8de4d0cc09f2bab3ca13c9fdc055
SpreeHertz <spreehertzofficial@gmail.com>
2022-06-13 17:58:21 +0530

    feat: add ability to turn off/on features

--- src/functions/mobsToKill.js
+++ src/functions/mobsToKill.js
@@ -1,15 +1,20 @@
 const bot = require('../../index');
 const pvp = require('mineflayer-pvp').plugin;
-bot.loadPlugin(pvp);
+const isFeatureEnabled = require('../../features.json');
 
-bot.on('spawn', async () => {
+if (isFeatureEnabled.mobsToKill === true) {
+	bot.loadPlugin(pvp);
 
-    // Give itself a sword
-    bot.chat(`/give ${bot.username} iron_sword 1`)
-	setInterval(() => {
-		const mobFilter = e => e.type === 'mob' && e.mobType === "Zombie" || e.mobType === 'Skeleton';
-		const mob = bot.nearestEntity(mobFilter);
-		if (!mob) return;
-		bot.pvp.attack(mob);
-	}, 2000);
-});
+	bot.on('spawn', async () => {
+	
+		// Give itself a sword
+		bot.chat(`/give ${bot.username} iron_sword 1`)
+		setInterval(() => {
+			const mobFilter = e => e.type === 'mob' && e.mobType === "Zombie" || e.mobType === 'Skeleton';
+			const mob = bot.nearestEntity(mobFilter);
+			if (!mob) return;
+			bot.pvp.attack(mob);
+		}, 2000);
+	});	
+}
+else return;
