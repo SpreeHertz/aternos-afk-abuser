1ac19182d6391fab74bb43bc8b5b47da4cdf9256
SpreeHertz <spreehertzofficial@gmail.com>
2022-06-13 21:25:49 +0530

    feat: new port

--- src/functions/mobsToKill.js
+++ src/functions/mobsToKill.js
@@ -6,15 +6,17 @@ if (isFeatureEnabled.mobsToKill === true) {
 	bot.loadPlugin(pvp);
 
 	bot.on('spawn', async () => {
-	
+
 		// Give itself a sword
-		bot.chat(`/give ${bot.username} iron_sword 1`)
+		bot.chat(`/give ${bot.username} iron_sword 1`);
 		setInterval(() => {
 			const mobFilter = e => e.type === 'mob' && e.mobType === "Zombie" || e.mobType === 'Skeleton';
 			const mob = bot.nearestEntity(mobFilter);
 			if (!mob) return;
 			bot.pvp.attack(mob);
 		}, 2000);
-	});	
+	});
 }
-else return;
+else {
+	return;
+}
